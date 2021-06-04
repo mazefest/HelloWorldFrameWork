9c43ad92ef6caa0b369e4d3633a2234b70ba81a3
Mazefest <colbymehmen@gmail.com>
2021-06-04 10:50:11 -0500

    Removed stuff

---
+++ HelloWorldFramework/HelloComputer.swift
@@ -0,0 +1,19 @@
+//
+//  HelloComputer.swift
+//  HelloWorldFramework
+//
+//  Created by Colby Mehmen on 6/3/21.
+//
+
+import Foundation
+
+public class HelloComputer {
+
+    public class func getHelloWorld() -> String {
+        return "hello world"
+    }
+    
+    public func printHello() {
+        Swift.print("hello")
+    }
+}
